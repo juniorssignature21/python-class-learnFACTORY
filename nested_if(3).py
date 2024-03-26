2ce3d15da4499504794ee26ff83bd6c0822d7972
Junior's Signature <chibuzor.john.2018@gmail.com>
2024-03-26 13:02:41 +0100

    added loops

---
+++ nested_if(3).py
@@ -0,0 +1,13 @@
+
+num  = int(input("Input a number: "))
+print("The number you inputed is ",num)
+if num%2==0:
+    if num%3==0:
+        if num%4==0:
+            
+    
+else:
+    print(num," is not a prime number")
+
+
+
